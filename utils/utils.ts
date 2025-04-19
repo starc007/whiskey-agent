74d60b7e058b26f3df000dce486e1a5764184348
Saurabh <infinityincorporative51@gmail.com>
2025-04-19 18:40:37 +0530

    fix: minor change

--- utils/utils.ts
+++ utils/utils.ts
@@ -6,31 +6,3 @@ import { RecommendationEngine } from "./recommendation-engine";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export async function analyzeUserBarAndGetRecommendations(
-  username: string,
-  recommendationLimit: number = 5
-) {
-  try {
-    // Fetch user's bar
-    const userBar = await BaxusService.getUserBar(username);
-
-    // Analyze the collection
-    const analysis = BaxusService.analyzeCollection(userBar);
-
-    // Get recommendations
-    const recommendations = await RecommendationEngine.getRecommendations(
-      userBar,
-      recommendationLimit
-    );
-
-    return {
-      analysis,
-      recommendations,
-      userBar,
-    };
-  } catch (error) {
-    console.error("Error analyzing bar and getting recommendations:", error);
-    throw error;
-  }
-}
